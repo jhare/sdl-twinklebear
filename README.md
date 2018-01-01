c7ec278c07e369a391561c0cde5e23e9fd147d7e
Joe Hare <joseph.hare@gmail.com>
2018-01-01 15:27:30 -0600

    moarmoarmoar

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 * for c++11 make sure to supply `-std=c++11`
 * make sure to include -Iinclude getResourcePath.h
+* I went ahead and did `sudo apt-get install libsdl2-*`, which will grab dev, plus a bunch of shit we don't need
--- lesson0/main.cpp
+++ lesson0/main.cpp
@@ -5,8 +5,8 @@
 /*
  * Lesson 0: Test to make sure SDL is setup properly
  */
-int main(int, char**){
-	if (SDL_Init(SDL_INIT_VIDEO) != 0){
+int main(int, char**) {
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
 		return 1;
 	}
